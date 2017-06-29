d35afa2b7ab0c8b6b3c3e8e022257d84d020d686
derpachu <nomnom148>
2017-06-29 18:27:29 -0400

    fixed some of the smaller issues with the text

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # derpachu.github.io
+
+this is creating a version 2 of the website that was previously done for a class for fall semester 2016.
+
+the scope of this new website is to fix all the flex box issues in the previous version as well as updating content.
+
+the goal is to make a presentable site that I can publish as well as assemble a protfolio to employers.
--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
-function emailpopout() 
-{
-    var popup = document.getElementById('email');
-    popup.classList.toggle('show');
+function emailpopout () {
+  var popup = document.getElementById('email')
+  popup.classList.toggle('show')
 }
-function mailingpopout() 
-{
-    var popup = document.getElementById('mailing');
-    popup.classList.toggle('show');
+function mailingpopout () {
+  var popup = document.getElementById('mailing')
+  popup.classList.toggle('show')
 }
